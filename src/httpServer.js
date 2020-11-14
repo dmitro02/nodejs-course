63dd656d0e3af4a00a1aeddf9bf878c461b3e90d
Dmitry <dmitro02@gmail.com>
2020-11-14 21:11:59 +0200

    http server WIP

--- src/httpServer.js
+++ src/httpServer.js
@@ -1,41 +1,91 @@
 const http = require('http')
-const path = require('path')
-const {
-  readFileSync,
-  writeFileSync,
-  read
-} = require('fs')
 
 const port = 8000
 
-const dataFile = path.join(__dirname, '../calendar-data.csv')
+const getRequestParams = (req) => {
+  let requestParams
+  const paramString = req.url.split('?')[1]
+  if (paramString) {
+    requestParams = {}
+    paramString.split('&').forEach((it) => {
+      const [ key, value ] = it.split('=')
+      if (key) requestParams[key] = value ? value : null
+    })
+  }
+  return requestParams
+} 
+
+const getEvents = (req, res) => {
+  const params = getRequestParams(req)
+  const events = params 
+    ? `Events filtered by parameters: ${JSON.stringify(params)}` 
+    : 'All events'
+  res.writeHead(200)
+  res.end(events)
+}
 
 const getEventById = (req, res) => {
   const id = req.url.split('/')[2]
-  const events = readFileSync(dataFile).toString().split('\n')
-  const event = events.find((it) => it.split(',')[0] === id)
   res.writeHead(200)
-  res.end(event)
+  res.end('event' + id)
+}
+
+const getUsers = (req, res) => {
+  const params = getRequestParams(req)
+  const users = params 
+    ? `Users filtered by parameters: ${JSON.stringify(params)}` 
+    : 'All users'
+  res.writeHead(200)
+  res.end(users)
+}
+
+const getUserById = (req, res) => {
+  const id = req.url.split('/')[2]
+  res.writeHead(200)
+  res.end('user' + id)
+}
+
+const createEvent = (req, res) => {
+  res.writeHead(201)
+  res.end('Event created')
+}
+
+const createUser = (req, res) => {
+  res.writeHead(201)
+  res.end('User created')
+}
+
+const getMainPage = (req, res) => {
+  res.writeHead(200)
+  res.end('<h1>Main Page</h1>')
 }
 
 const notFoundHandler = (req, res) => {
   res.writeHead(404)
-  res.end()
+  res.end('<h1>Not found</h1>')
 } 
 
-const routes = [
-  [/\/events\/\d+/, getEventById]
-]
-
-const getRouteHandler = (req) => {
-  const handler = routes.find((it) => it[0].test(req.url))
-  return handler ? handler[1] : notFoundHandler
+const routes = {
+  GET: [
+    [ /\/events\/\d+/, getEventById ],
+    [ /\/users\/\d+/, getUserById ],
+    [ /\/events/, getEvents ],
+    [ /\/users/, getUsers ],
+    [ /\/.+/, notFoundHandler ],
+    [ /\//, getMainPage ]
+  ],
+  POST: [
+    [ /\/events/, createEvent ],
+    [ /\/users/, createUser ],
+    [ /\/.+/, notFoundHandler ]
+  ]
 }
-  
+
 const server = http.createServer((req, res) =>
-  getRouteHandler(req)(req, res)
+  routes[req.method.toUpperCase()]
+    .find((it) => it[0].test(req.url))[1](req, res)
 )
 
-server.listen(port, () => {
+server.listen(port, () =>
   console.log(`Server is running on port ${port}`)
-})
+)
